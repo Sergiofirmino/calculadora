795358eab50db9b96d664b552ce803cb70f9c2cc
Sergio Firmino <43519570+Sergiofirmino@users.noreply.github.com>
2024-07-13 18:29:41 -0300

    Versao 2.0

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function clearDisplay() {
-    document.getElementById('display').value = '';
-}
+document.getElementById("clear").addEventListener("click", () => document.getElementById('display').value = '');
 
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
-}
 
-function calculateResult() {
+document.querySelectorAll(".number, .operation").forEach(button => {
+    button.addEventListener("click", () => 
+        document.getElementById('display').value += button.getAttribute("data-value"));
+});
+
+document.getElementById("equals").addEventListener("click", () => {
     const display = document.getElementById('display');
     try {
         display.value = eval(display.value);
     } catch (error) {
         display.value = 'Erro';
     }
-}
+});
